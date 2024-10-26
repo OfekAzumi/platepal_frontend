cf2ede3998dd6d87232313d604048f5ce3338189
תומר עזומי <OfekAzumi>
2024-10-26 20:18:46 +0300

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,17 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      // Set unused variables to warn instead of error in CI
+      "no-unused-vars": process.env.CI ? "warn" : "error",
+      // Other rule overrides if needed
+    },
+  },
 ];
